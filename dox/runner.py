b2b690edccb0954f0dd742e810fe484e3bc8ed97
Monty Taylor <mordred@inaugust.com>
2014-07-26 21:54:40 -0700

    Have useradd create the group itself

--- dox/runner.py
+++ dox/runner.py
@@ -117,8 +117,7 @@ class Runner(object):
         try:
             tempd = tempfile.mkdtemp()
             dockerfile.append(
-                "RUN groupadd -g %(gid)s %(user)s"
-                " && useradd -M -d /src -g %(gid)s -u %(uid)s %(user)s" % dict(
+                "RUN useradd -M -U -d /src -u %(uid)s %(user)s" % dict(
                     uid=os.getuid(), gid=os.getgid(), user=os.getlogin()))
             for add_file in commands.get_add_files():
                 shutil.copy(add_file, os.path.join(tempd, add_file))
