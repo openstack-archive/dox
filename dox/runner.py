2b99274bb41348fe79ffdabff780cac239868664
Andrew White <white.d.andrew@gmail.com>
2016-04-06 10:17:16 -0400

    Fixed docker image name mangling
    Double underscrores could result with docker image name scheme used runner.py. These are invalid in some Docker versions.

--- dox/runner.py
+++ dox/runner.py
@@ -33,12 +33,12 @@ logger = logging.getLogger(__name__)
 class Runner(object):
 
     def __init__(self, args, image_name=None):
-        image_name = image_name and "/" + image_name or ""
+        image_name = image_name and image_name or ""
         self.args = args
         self.project = os.path.basename(os.path.abspath('.'))
-        self.base_image_name = 'dox/%s%s_base' % (self.project,
+        self.base_image_name = 'dox/%s%s-base' % (self.project,
                                                   image_name)
-        self.test_image_name = 'dox/%s%s_test' % (self.project,
+        self.test_image_name = 'dox/%s%s-test' % (self.project,
                                                   image_name)
         self.user_map = self._get_user_mapping()
         self.path_map = self._get_path_mapping()
