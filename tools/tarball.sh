fcfeca395cf87487fb98fd51a84330346b4affbb
Paul Belanger <pabelanger@redhat.com>
2017-04-27 12:19:16 -0400

    [WIP] Ensure we can build a docker image
    Change-Id: I1f817b9154bfebe87b5c66291e70bc8fc10f203a
    Signed-off-by: Paul Belanger <pabelanger@redhat.com>

---
+++ tools/tarball.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -ex
+
+GROUP=docker
+if [ $(id -gn) != ${GROUP} ]; then
+  sudo gpasswd -a ${USER} ${GROUP}
+  sudo service docker restart
+  exec sg ${GROUP} "$0 $*"
+fi
+
+### Build image with docker
+IMAGES="infra/centos7 infra/trusty"
+for IMAGE in $IMAGES; do
+    docker build dockerfiles/$IMAGE -t $IMAGE
+done
+
+# NOTE(pabelanger): Make sure we hash by ZUUL_COMMIT, so we know which tarball
+# to download from secure worker.
+DIST=$WORKSPACE/dist/$ZUUL_COMMIT
+mkdir -p $DIST
+
+### Save docker image for upload to tarballs.o.o
+FILENAME=images.tar.gz
+docker save $IMAGES | gzip -9 > $DIST/$FILENAME
+shasum $DIST/$FILENAME > $DIST/$FILENAME.sha256
+
+### Mock trusted worker
+docker rmi -f $IMAGES
+docker images
+
+shasum -c $DIST/$FILENAME.sha256
+docker load < $DIST/$FILENAME
+
+docker images
